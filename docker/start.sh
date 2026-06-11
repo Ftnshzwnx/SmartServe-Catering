e0a6c2d6cd0d3f40dac69d78d776371dccb978b5
Ftnshzwnx <fshazwina223@gmail.com>
2026-06-11 20:14:57 +0800

    fix: set log channel to stderr and fix container storage/cache permissions for www-data

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Force Laravel logs to stderr so they appear in Railway console
+export LOG_CHANNEL=stderr
+
 echo "=== Setting up Laravel ==="
 
 # Generate APP_KEY if not set
@@ -23,6 +26,9 @@ php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+# Set permissions for www-data since artisan commands ran as root
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 
 echo "=== Starting services ==="
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
