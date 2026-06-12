8cd4bf1839c2655d048dd6da2e2109dc7415630f
Ftnshzwnx <fshazwina223@gmail.com>
2026-06-12 16:54:49 +0800

    feat: make Google Maps dynamic based on business address setting

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -51,6 +51,11 @@ const handleWhatsAppClick = () => {
     const url = `https://wa.me/${phoneNum}?text=${encodeURIComponent(text)}`;
     window.open(url, '_blank');
 };
+
+const mapUrl = computed(() => {
+    const address = page.props.settings.business_address || 'Gong Badak, Kuala Terengganu, Terengganu, Malaysia';
+    return `https://maps.google.com/maps?q=${encodeURIComponent(address)}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+});
 </script>
 
 <template>
@@ -339,7 +344,7 @@ const handleWhatsAppClick = () => {
         <!-- Google Maps Embed Placeholder -->
         <section class="h-[400px] bg-[#EADED9] relative overflow-hidden border-t border-[#E6E1DA]">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.038977857766!2d103.1363!3d5.3302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31b6ba2b8344e13d%3A0x7d02ebbd3006eb49!2sGong+Badak%2C+Kuala+Terengganu%2C+Terengganu!5e0!3m2!1sen!2smy!4v1700000000000"
+                :src="mapUrl"
                 class="w-full h-full"
                 style="border:0;"
                 allowfullscreen=""
